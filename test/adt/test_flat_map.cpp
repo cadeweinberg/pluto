61a1958b75b72d939e3a3985a118c9429844dfe5
Cade Weinberg <cadeweinberg@gmail.com>
2026-04-28 10:51:49 -0700

    test: strengthened test_flat_map

--- test/adt/test_flat_map.cpp
+++ test/adt/test_flat_map.cpp
@@ -1,24 +1,18 @@
 // SPDX-Identifier: GPL-3.0-or-later
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
 
 #include "adt/flat_map.hpp"
 
 TEST_CASE("FlatMap can insert and retrieve values", "[FlatMap]") {
     pluto::FlatMap<int, std::string> map;
 
-    std::vector<std::pair<int, std::string>> entries = {
-        {1, "one"},
-        {2, "two"},
-        {3, "three"},
-        {4, "four"},
-        {5, "five"},
-        {6, "six"},
-        {7, "seven"},
-        {8, "eight"},
-        {9, "nine"},
-        {10, "ten"},
-    };
+    std::vector<std::pair<int, std::string>> entries;
+
+    for (int i = 0; i < 256; ++i) {
+        entries.emplace_back(i, std::to_string(i));
+    }
 
     for (const auto &[key, value] : entries) {
         auto &entry = map.at(key);
@@ -28,5 +22,9 @@ TEST_CASE("FlatMap can insert and retrieve values", "[FlatMap]") {
     for (const auto &[key, value] : entries) {
         REQUIRE(map.at(key).second == value);
     }
+
+    // BENCHMARK("FlatMap.at(key)") {
+    //     return map.at(64).second;
+    // };
 }
 
